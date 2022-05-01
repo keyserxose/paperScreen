62cd0c4505ba72c02f9ee8fb853e466dae9e1c15
drusklo <drusklo5@gmail.com>
2022-05-01 20:28:49 +0200

    some changes

--- htmlToimage.py
+++ htmlToimage.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os
 from html2image import Html2Image
 hti = Html2Image()
 
-hti.screenshot(url='http://localhost:8080/data.json', save_as='status.png', size=(600, 800))
+# Take the screenshot
+hti.screenshot(url='http://localhost:8080/image.png', save_as='status.png', size=(600, 800))
+
+# Convert to grayscale
+os.system('convert status.png -type GrayScale -depth 8 -colors 256 status.png')
+
+os.system('chmod 777 status.png')
+
+# Copy image to apache dir
+os.system('cp -p status.png /srv/http/status.png')
